d371926d1af65bbded6fd7f73f0e378cae0ae537
Amitava Roy <royamitava1988@gmail.com>
2026-06-24 18:29:54 +0530

    feature branch first commit

---
+++ scripts/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Starting test script..."
+echo "Listing files in app folder:"
+ls -l app
+
+echo "Displaying content of hello.txt"
+cat app/hello.txt
+
+echo "Test completed successfully."
